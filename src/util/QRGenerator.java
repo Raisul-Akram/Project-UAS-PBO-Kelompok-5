b9ae72ef62168ef5854f127e4defa4fcb5b89bfd
keiveenaldiandra <keiveenaldiandra23utama@gmail.com>
2025-11-24 21:08:14 +0700

    QRGenerator.java

--- src/util/QRGenerator.java
+++ src/util/QRGenerator.java
@@ -4,3 +4,8 @@ package util;
  * Generator QR Code ASCII sederhana untuk kode booking.
  * @author Kelompok 5
  */
+
+public class QRGenerator {
+    public static void generateQR(String code) {
+        System.out.println("\n\033[32m=== QR Code untuk Booking: " + code + " ===\033[0m");
+        // ASCII art sederhana untuk QR (placeholder)
