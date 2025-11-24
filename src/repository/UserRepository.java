cfb3cd62f0ab7ac6c6bd24d98c0460d9713b738f
Nayla Khansa Livya <naylakl2406@gmail.com>
2025-11-24 16:43:19 +0700

    Add UserRepository class
    Initialize UserRepository with a dummy data loader.

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,1 +1,15 @@
+package repository;
 
+import model.User;
+import model.Admin;
+import model.Account;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+
+    private List<Account> users = new ArrayList<>();
+
+    public UserRepository() {
+        loadDummyData();
+    }
