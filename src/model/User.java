5c6a49ea52c2b3b2b4cc3a890ff15d00d371dc7f
Nayla Khansa Livya <naylakl2406@gmail.com>
2025-11-24 16:37:32 +0700

    Update User.java

--- src/model/User.java
+++ src/model/User.java
@@ -4,4 +4,9 @@ public class User extends Account {
     public User(String id, String username, String password) {
         super(id, username, password);
     }
-
+    
+    @Override
+    public void showMenu() {
+        System.out.println("User Menu");
+    }
+}
