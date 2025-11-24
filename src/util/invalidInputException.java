70fd9dfd87c549b01f39de954cdcc2b7b8e95f76
Raisul akram <akramraisul4@gmail.com>
2025-11-24 16:22:58 +0700

    InvalidInputException

---
+++ src/util/invalidInputException.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * Exception untuk input tidak valid.
+ * @author Kelompok 5
+ */
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
