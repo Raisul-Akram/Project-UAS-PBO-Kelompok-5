463b51551f2f278b2addb106e47349e3bb669475
Nayla Khansa Livya <naylakl2406@gmail.com>
2025-11-24 16:46:34 +0700

    Add AuthService class with UserRepository initialization

--- src/auth/AuthService.java
+++ src/auth/AuthService.java
@@ -1,1 +1,14 @@
+package auth;
 
+import model.Account;
+import model.Admin;
+import model.User;
+import repository.UserRepository;
+
+public class AuthService {
+
+    private UserRepository userRepo;
+
+    public AuthService() {
+        this.userRepo = new UserRepository();
+    }
