5487f7f7a8c670f6042dfbb6717fb79aec7cccf0
cutwynona <wynonacut@gmail.com>
2025-11-25 12:55:37 +0700

    Update Movie.java

--- src/model/Movie.java
+++ src/model/Movie.java
@@ -1,28 +1,32 @@
 package model;
 
-/**
- * Class untuk menyimpan data film.
- * @author Kelompok 5
- */
 public class Movie {
     private String id;
     private String title;
     private String genre;
-    private int popularity; // Jumlah pemesan
+    private int duration; // Tambahan baru
 
-    public Movie(String id, String title, String genre) {
+    // Update Constructor: Terima 4 parameter
+    public Movie(String id, String title, String genre, int duration) {
         this.id = id;
         this.title = title;
         this.genre = genre;
-        this.popularity = 0;
+        this.duration = duration;
     }
 
-    // Getters & Setters
+    // Constructor lama (untuk jaga-jaga kompatibilitas, optional)
+    public Movie(String id, String title, String genre) {
+        this(id, title, genre, 0);
+    }
+
+    // Getters
     public String getId() { return id; }
     public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
     public String getGenre() { return genre; }
+    public int getDuration() { return duration; } // Tambahan baru
+    
+    // Setters (jika perlu)
+    public void setTitle(String title) { this.title = title; }
     public void setGenre(String genre) { this.genre = genre; }
-    public int getPopularity() { return popularity; }
-    public void incrementPopularity() { this.popularity++; }
+    public void setDuration(int duration) { this.duration = duration; }
 }
